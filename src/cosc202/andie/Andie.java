183d4c1fe3135be050f7d20fdef78d7b1d39b3d7
Jeb Nicholson <26765018+jeb5@users.noreply.github.com>
2023-03-14 12:32:58 +1300

    Changed frame name

--- src/cosc202/andie/Andie.java
+++ src/cosc202/andie/Andie.java
@@ -51,7 +51,7 @@ public class Andie {
      */
     private static void createAndShowGUI() throws Exception {
         // Set up the main GUI frame
-        JFrame frame = new JFrame("ANDIE");
+        JFrame frame = new JFrame("Photoshop 2024");
 
         Image image = ImageIO.read(Andie.class.getClassLoader().getResource("icon.png"));
         frame.setIconImage(image);
