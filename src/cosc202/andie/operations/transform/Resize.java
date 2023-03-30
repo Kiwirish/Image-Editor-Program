3f67cdcc3f199ad00b97fc1d910bf9e2c608baed
Bernard Pieters <Piebe649@student.otago.ac.nz>
2023-03-31 12:12:20 +1300

    5

--- src/cosc202/andie/operations/transform/Resize.java
+++ src/cosc202/andie/operations/transform/Resize.java
@@ -1,16 +1,29 @@
 package cosc202.andie.operations.transform;
 
+import java.awt.Graphics2D;
 import java.awt.image.*;
 
 import cosc202.andie.ImageOperation;
 
 public class Resize implements ImageOperation, java.io.Serializable{
 
-    public Resize(){}
+    private int option;
+
+    public Resize(int option){
+        this.option = option;
+    }
 
     public BufferedImage apply(BufferedImage input){
-        BufferedImage result = (BufferedImage) input.getScaledInstance(1, 1, 1);
-        return result;
+        
+        int scaledWidth = (int)(input.getWidth() * ((float)option/100));
+        int scaledHeight = (int)(input.getHeight() * ((float)option/100));
+
+        BufferedImage output = new BufferedImage(scaledWidth, scaledHeight, input.getType());
+        
+        Graphics2D graphics2D = output.createGraphics();
+        graphics2D.drawImage(input,0,0,scaledWidth , scaledHeight, null);
+
+        return output;
     }
-    
+
 }
